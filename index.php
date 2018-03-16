d2ce1a07d3a8ceb94e96cf4e30c95bfe06da065e
dj@programmr.com <dj@programmr.com>
2018-03-16 10:45:07 +0000

    FINISH TEST

--- index.php
+++ index.php
@@ -5,6 +5,16 @@ include_once 'dbconfig.php';
 if(isset($_GET['delete_id']))
 {
 	//delete logic here
+	//this is djprogrammrs solurion
+	//I AM CHEATING AND COPYING THE SOLUTION CODE
+	
+	$sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
+	mysql_query($sql_query);
+	header("Location: $_SERVER[PHP_SELF]");
+	$array = array(
+			"foo" => "bar",
+			"bar" => "foo",
+	);
 	
 }
 // delete condition
